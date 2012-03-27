4f2050ae575e0c142ec68b56479e7df3f264ce04
Sam Tobin-Hochstadt <samth@racket-lang.org>
2012-03-27 18:40:21 -0400

    Add mac support (untested).

--- run-dates.rkt
+++ run-dates.rkt
@@ -4,12 +4,17 @@
 
 (define num-dates 5)
 
-(define lib-path "libwarp.so.1.0")
-
-(define arch-path 
-  (if (fixnum? (expt 2 40))
-      "linux-amd64"
-      "linux-i386"))
+(define lib-path 
+  (case (system-type)
+    [(unix)
+     (if (fixnum? (expt 2 40))
+         "linux-amd64/libwarp.so.1.0"
+         "linux-i386/libwarp.so.1.0")]
+    [(macosx)
+     (if (fixnum? (expt 2 40))
+         "mac-amd64/libwarp.dylib.1.0"
+         "mac-i386/libwarp.dylib.1.0")]
+    [else (error 'run-date "warp is not supported on windows")]))
 
 (command-line
  #:once-each
@@ -21,5 +26,5 @@
 
 (for ([i l])
   (system 
-   (format "WARP=~a LD_PRELOAD=~a/~a racket -l meta/build/test-drracket" 
-           i arch-path lib-path)))
+   (format "WARP=~a LD_PRELOAD=~a racket -l meta/build/test-drracket" 
+           i lib-path)))
